6abe32444e9675fb5a1ba8004f4d70284fab3e44
Stephane <stephane.hua1997@gmail.com>
2023-09-21 23:19:35 +0200

    #1.2 : add User model and repository

--- back/src/main/java/com/openclassrooms/mdd/mddapp/models/User.java
+++ back/src/main/java/com/openclassrooms/mdd/mddapp/models/User.java
@@ -1,4 +1,59 @@
 package com.openclassrooms.mdd.mddapp.models;
 
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
+@Data
+@Accessors(chain = true)
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(of = {"id"})
+@Builder
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@ToString
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NonNull
+    @Size(max = 50)
+    @Column(name = "name")
+    private String name;
+
+
+    @NonNull
+    @Size(max = 100)
+    private String password;
+
+    @NonNull
+    @Column(name = "admin")
+    private Boolean admin;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
 }
--- back/src/main/java/com/openclassrooms/mdd/mddapp/repositories/UserRepository.java
+++ back/src/main/java/com/openclassrooms/mdd/mddapp/repositories/UserRepository.java
@@ -1,4 +1,13 @@
 package com.openclassrooms.mdd.mddapp.repositories;
 
-public class UserRepository {
+
+import com.openclassrooms.mdd.mddapp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
 }
