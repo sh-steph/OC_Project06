db163482ddac03012a8f648c6a1eafb4f185d22b
Stephane <stephane.hua1997@gmail.com>
2023-11-23 21:40:01 +0100

    #2.4 : Update theme list page by making it responsive

--- front/src/app/pages/theme-list/theme-list.component.ts
+++ front/src/app/pages/theme-list/theme-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, Subject, of, takeUntil } from 'rxjs';
+import { SubscriptionResponse } from 'src/app/interfaces/subscription.interface';
 import { Theme, ThemeList } from 'src/app/interfaces/theme.interface';
+import { SubscriptionsService } from 'src/app/services/api/subscriptions.service';
 import { ThemesService } from 'src/app/services/api/themes.service';
 
 @Component({
@@ -14,19 +16,19 @@ export class ThemeListComponent implements OnInit, OnDestroy {
   onArticleTab: boolean = false;
   onUserTab: boolean = false;
 
-  public getAllPosts: Observable<ThemeList | null> = of(null);
+  public getAllThemes: Observable<ThemeList | null> = of(null);
   private destroy = new Subject<void>();
 
   themeList: ThemeList = { themes: [] };
 
-  constructor(private themesService: ThemesService, private router: Router) {}
+  constructor(private themesService: ThemesService, private subscriptionsService: SubscriptionsService,private router: Router) {}
 
   ngOnInit(): void {
     this.onArticleTab = false;
     this.onThemeTab = true;
     this.onUserTab = false;
-    this.getAllPosts = this.themesService.getAllTheme();
-    this.getAllPosts.pipe(takeUntil(this.destroy)).subscribe((themesData) => {
+    this.getAllThemes = this.themesService.getAllTheme();
+    this.getAllThemes.pipe(takeUntil(this.destroy)).subscribe((themesData) => {
       // wait to get data from subscribe
       if (themesData) {
         this.themeList = themesData;
@@ -43,5 +45,10 @@ export class ThemeListComponent implements OnInit, OnDestroy {
     return themes.slice().sort((a, b) => a.title.localeCompare(b.title));
   }
 
-  subscribeButton() {}
+  subscribeButton(id: number): void {
+    const themeId = String(id);
+    this.subscriptionsService
+    .addSubscription(themeId)
+    .subscribe((subscriptionResponse: SubscriptionResponse) => subscriptionResponse)
+  }
 }
