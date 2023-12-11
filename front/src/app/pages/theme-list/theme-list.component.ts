91bc2580146aaab69f64118bb80e1bcf8c380ad8
Stephane <stephane.hua1997@gmail.com>
2023-12-12 00:49:00 +0100

    2.4 : update the theme list component by adding a disable button for the subscribed theme

--- front/src/app/pages/theme-list/theme-list.component.ts
+++ front/src/app/pages/theme-list/theme-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
 import { Observable, Subject, of, takeUntil } from 'rxjs';
-import { SubscriptionResponse } from 'src/app/interfaces/subscription.interface';
+import {
+  SubscriptionList,
+  SubscriptionResponse,
+} from 'src/app/interfaces/subscription.interface';
 import { Theme, ThemeList } from 'src/app/interfaces/theme.interface';
 import { SubscriptionsService } from 'src/app/services/api/subscriptions.service';
 import { ThemesService } from 'src/app/services/api/themes.service';
@@ -15,18 +17,33 @@ export class ThemeListComponent implements OnInit, OnDestroy {
   onThemeTab: boolean = false;
   onArticleTab: boolean = false;
   onUserTab: boolean = false;
+  subscribedList: ThemeList = { themes: [] };
 
   public getAllThemes: Observable<ThemeList | null> = of(null);
+  public getAllSubscriptions: Observable<SubscriptionList | null> = of(null);
   private destroy = new Subject<void>();
 
   themeList: ThemeList = { themes: [] };
 
-  constructor(private themesService: ThemesService, private subscriptionsService: SubscriptionsService,private router: Router) {}
+  constructor(
+    private themesService: ThemesService,
+    private subscriptionsService: SubscriptionsService
+  ) {}
 
   ngOnInit(): void {
     this.onArticleTab = false;
     this.onThemeTab = true;
     this.onUserTab = false;
+    this.getThemesList();
+    this.getSubscribedList();
+  }
+
+  ngOnDestroy() {
+    this.destroy.next();
+    this.destroy.unsubscribe;
+  }
+
+  getThemesList() {
     this.getAllThemes = this.themesService.getAllTheme();
     this.getAllThemes.pipe(takeUntil(this.destroy)).subscribe((themesData) => {
       // wait to get data from subscribe
@@ -36,9 +53,24 @@ export class ThemeListComponent implements OnInit, OnDestroy {
     });
   }
 
-  ngOnDestroy() {
-    this.destroy.next();
-    this.destroy.unsubscribe;
+  getSubscribedList() {
+    this.getAllSubscriptions =
+      this.subscriptionsService.getAllSubscriptionFromUser();
+    this.getAllSubscriptions
+      .pipe(takeUntil(this.destroy))
+      .subscribe((subscriptionData) => {
+        // wait to get data from subscribe
+        if (subscriptionData) {
+          const themes: Theme[] = subscriptionData.subscriptions.map(
+            (subscription) => subscription.theme
+          );
+          this.subscribedList = { themes: themes };
+        }
+      });
+  }
+
+  isThemeSubscribed(themeId: number): boolean {
+    return this.subscribedList.themes.some((theme) => theme.id === themeId);
   }
 
   sortThemesByAlphabetical(themes: Theme[]): Theme[] {
@@ -48,7 +80,9 @@ export class ThemeListComponent implements OnInit, OnDestroy {
   subscribeButton(id: number): void {
     const themeId = String(id);
     this.subscriptionsService
-    .addSubscription(themeId)
-    .subscribe((subscriptionResponse: SubscriptionResponse) => subscriptionResponse)
+      .addSubscription(themeId)
+      .subscribe((subscriptionResponse: SubscriptionResponse) => {
+        location.reload();
+      });
   }
 }
