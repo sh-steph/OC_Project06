f3beb525740e1cd9ba15597a28b3c9eaaf0e0687
Stephane <stephane.hua1997@gmail.com>
2023-12-12 00:53:33 +0100

    # 2.7 : add userDetail service to call his api

---
+++ front/src/app/interfaces/userDetail.interface.ts
@@ -0,0 +1,12 @@
+export interface UserDetail {
+  email: string;
+  username: string;
+}
+
+export interface UserDetailRequest {
+  message: String;
+}
+
+export interface UserDetailResponse {
+  message: String;
+}
