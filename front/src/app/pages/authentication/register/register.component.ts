1d660eaa9500a60ee6d9c4d634199db6a5936fdc
Stephane <stephane.hua1997@gmail.com>
2023-12-12 00:41:55 +0100

    #2.1 : update register form by adding a control in the register process

--- front/src/app/pages/authentication/register/register.component.ts
+++ front/src/app/pages/authentication/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable, Subject, of, takeUntil } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
 import { RegisterRequest } from 'src/app/interfaces/registerRequest.interface';
 import { AuthService } from 'src/app/services/authentication/auth.service';
 import { SessionService } from 'src/app/services/session.service';
@@ -12,13 +12,12 @@ import { SessionService } from 'src/app/services/session.service';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit, OnDestroy {
-
   loggedBool: Observable<boolean | null> = of(null);
   userConnected: boolean = false;
   private destroy = new Subject<void>();
 
   public onError = false;
-  messageError: String = "";
+  messageError: String = '';
 
   public form = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -28,7 +27,12 @@ export class RegisterComponent implements OnInit, OnDestroy {
     ],
     password: [
       '',
-      [Validators.required, Validators.min(3), Validators.max(40)],
+      [
+        Validators.required,
+        Validators.min(8),
+        Validators.max(40),
+        this.validatePasswordCondition,
+      ],
     ],
   });
 
@@ -41,20 +45,19 @@ export class RegisterComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.loggedBool = this.sessionService.$isLogged();
-    this.loggedBool
-    .subscribe((logged) => {
-        if (logged) {
-          this.userConnected = logged;
-          this.router.navigate(['/postList']);
-        } else {
-          this.userConnected = false;
-        }
-      });
+    this.loggedBool.subscribe((logged) => {
+      if (logged) {
+        this.userConnected = logged;
+        this.router.navigate(['/postList']);
+      } else {
+        this.userConnected = false;
+      }
+    });
   }
 
   ngOnDestroy() {
-    this.destroy.next()
-    this.destroy.unsubscribe
+    this.destroy.next();
+    this.destroy.unsubscribe;
   }
 
   public submit(): void {
@@ -63,12 +66,37 @@ export class RegisterComponent implements OnInit, OnDestroy {
       next: (_: void) => this.router.navigate(['/login']),
       error: (error) => {
         this.onError = true;
-        this.messageError = error.error.message
-      }
+        this.messageError = error.error.message;
+      },
     });
   }
 
   public buttonPreviousTab(): void {
     this.router.navigate(['/home']);
+  }
+
+  private validatePasswordCondition(
+    control: AbstractControl
+  ): { [key: string]: boolean } | null {
+    const value: string = control.value;
+    const errors: { [key: string]: boolean } = {};
+
+    const lowercaseRegex = /[a-z]/;
+    const uppercaseRegex = /[A-Z]/;
+    const digitRegex = /\d/;
+    const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+
+    errors['lowercase'] = !lowercaseRegex.test(value);
+    errors['uppercase'] = !uppercaseRegex.test(value);
+    errors['digit'] = !digitRegex.test(value);
+    errors['specialChar'] = !specialCharRegex.test(value);
+
+    const isValid = !Object.values(errors).some((error) => error);
+
+    if (!isValid) {
+      return { passwordCondition: true, ...errors };
+    }
+
+    return null;
   }
 }
